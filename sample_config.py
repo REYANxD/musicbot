b27fa1cd8c8a46a7651c36b5142f892cf337cef2
VASU-XD <79446681+VASU-XD@users.noreply.github.com>
2021-10-16 21:30:07 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -18,12 +18,12 @@ if HEROKU:
 
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
-    API_ID = 8841293
-    API_HASH = "77920a25c9bf9cc8c193ca6fe28352e7"
+    API_ID = 7475191
+    API_HASH = "32dbc9e8368cca754844f77399f6db81"
     ARQ_API_KEY = "TNMEAT-LJUCUR-NKKNHA-HSAXCC-ARQ"
     CHAT_ID = -1001517082431
     DEFAULT_SERVICE = "youtube"  # Must be one of "youtube"/"saavn"
     BITRATE = 512 # Must be 512/320
-    SESSION_STRING = "AQCLRG7I_Esiqdv2wYylRMPxNKnzfCQEcDIhHmYm9eO9ccmqDrBMecKz6xElXxCZ-cxm3FfyyJ0ABpeQJf9BQttW9yUrtvvyT7b5ApyhJ-ySsqNzqUeTCGUwhHEnGUkgSe-9zzkiK7Tgz_jIkX34nrZ028IGX-Q-Sia6IKWVS8Gfbw1Efs4x1ZZLIK418GVAXZt0DlK2mUnpwiSOceOZxJMrM3KB-Agc768OlBYdaRRLA2mNC5QyF2RE2UXS9sEtKl9ylu8rgGGPbV2zxxV-xyN8zE8kPFSSjnbquxJgcS19o5nxkt-F-BIeI6Q1Hg2BdQgT9V6ncoJqZSmN3y9eXfefeMiwvQA"
+    SESSION_STRING = "AQCxWYaFJXCpaAozF6OoH0foTNrRpC1pKKO3PWwF51KqkQYW7ZO_UgB5hl3mqi59VV1iyGPpZRbURvCp6VuX0IxpIywRpNsBAATEPLNHd7JCBLBhx3EEu9_rgT2o1p7BXuNAGPv9e3ANEGJwGxfTrog5pg55cPazNgRnBD2bsEbLDOnQ_zm6EXXnJ7pUw1IBySxXql33xUl6mvgh3sfyexQnI10SPKUjikMFkZf3dhhLlTvdapOS4Kqseqn2TYRI8-ajSEjyJDwWTow6h0zVZtYTwkYQDMlDSDgyLs5oQLhabjwR6ryt4rXtjZm9y5giOteD47l-DASpZ7rgtx5k-MAOcu_qiwA"
 # don't make changes below this line
 ARQ_API = "https://thearq.tech"
